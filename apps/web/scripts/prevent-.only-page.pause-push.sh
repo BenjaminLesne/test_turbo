f7650aee5b38a15cc8725fc3a6640ab62f4b5b2f
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-09-20 20:09:40 +0200

    wip husky

--- apps/web/scripts/prevent-.only-page.pause-push.sh
+++ apps/web/scripts/prevent-.only-page.pause-push.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if grep -R "test.only" ./tests; then
+if grep -R "test.only" ./e2e; then
     echo "🛑🛑🛑🛑 Push aborted: Remove 'test.only' from test files. 🛑🛑🛑🛑"
     exit 1
 fi
 
-if grep -R "page.pause" ./tests; then
+if grep -R "page.pause" ./e2e; then
     echo "🛑🛑🛑🛑 Push aborted: Remove 'page.pause' from test files. 🛑🛑🛑🛑"
     exit 1
 fi
