b7fc3f85e8c0c0ac23b3801f409e4faaff2ab2fe
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-10-01 17:31:34 +0200

    added @eslint-react/eslint-plugin

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,10 +1,20 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
+  plugins: ["@eslint-react/eslint-plugin"],
   extends: [
     "next/core-web-vitals",
     "@repo/eslint-config/base.cjs",
     "plugin:tailwindcss/recommended",
+  ],
+  overrides: [
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: ["plugin:@eslint-react/recommended-type-checked-legacy"],
+      rules: {
+        "@eslint-react/no-leaked-conditional-rendering": "error",
+      },
+    },
   ],
   settings: {
     react: {
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,16 @@
-import getInfo from "@/lib/actions";
+"use client";
 
-export default async function HomePage() {
-  const data = await getInfo()
-  const stuff = ""
+// import getInfo from "@/lib/actions";
+import { useEffect, useState } from "react";
+
+export default function HomePage() {
+  const [items, setItems] = useState([0, 1, 2, 3, 4]);
+  // const data = await getInfo();
+  const stuff = "";
+
+  useEffect(() => {
+    setItems((x) => [...x].reverse());
+  }, [items]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
