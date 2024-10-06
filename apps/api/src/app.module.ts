114eae8d1223512f84647715b2cdead6acc009f1
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-10-06 22:18:00 +0200

    wip

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { HealthModule } from "./health/health.module";
+import { ConfigModule } from "@nestjs/config";
+import { envConfig } from "./config/configuration";
 
 @Module({
-  imports: [HealthModule],
+  // imports: [
+  //   ConfigModule.forRoot({
+  //     isGlobal: true,
+  //     load: [envConfig],
+  //   }),
+  //   HealthModule,
+  // ],
   controllers: [AppController],
   providers: [AppService],
 })
