f58d488ab0d254079bd12226f60a00fcd66bb361
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-10-25 21:24:04 +0200

    wip

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,3 +6,24 @@ async function bootstrap() {
   await app.listen(3001);
 }
 void bootstrap();
+
+// TODO:
+// api/pnpm dev fail, I don't know why, first commit to fail below
+
+// b0f4b51
+
+// apps/api/.eslintrc.cjs | 4 ++--
+
+// apps/api/package.json | 2 ++
+
+// apps/api/src/health/health.controller.ts | 4 ++--
+
+// apps/api/tsconfig.json | 1 +
+
+// apps/web/.eslintrc.cjs | 4 ++--
+
+// packages/eslint-config/base.cjs | 16 ++++++++++++----
+
+// packages/eslint-config/package.json | 4 ++++
+
+// pnpm-lock.yaml
