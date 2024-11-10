d3faf45cd981d071ca22a45f0ef8b60146b45def
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-11-10 22:15:06 +0100

    wip

--- apps/web/src/features/feedback/components/feedback-form.tsx
+++ apps/web/src/features/feedback/components/feedback-form.tsx
@@ -1,3 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  // TODO: use a package for shared schemas between back and front?
+  // this formSchema should be the same as the backend schema validation of the body no?
+  message: z.string().min(2).max(50),
+});
+
 export const FeedbackForm = () => {
-  return <div>FeedbackForm</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea placeholder="Your feedback..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
